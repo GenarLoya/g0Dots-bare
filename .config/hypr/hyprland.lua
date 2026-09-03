95d4bde2e3765da7f2ba6198d7e1b5f6a94fcd76
Genaro Loya Dour <genarold@movil-2806-0370-306c-4ea0-87e1-740f-95b7-09e2.itelcel.com>
2026-09-03 00:22:36 -0600

    fix: async call

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -174,6 +174,6 @@ hl.gesture({
 hl.on("hyprland.start", function()
     hl.exec_cmd("waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css")
     hl.exec_cmd("~/.config/hypr/scripts/start-graphical-session.sh")
-    hl.exec_async("awww-daemon")
-    hl.exec_async("sleep 1 && ~/.config/hypr/scripts/random-wallpaper.sh")
+    hl.exec_cmd("awww-daemon")
+    hl.exec_cmd("sleep 1 && ~/.config/hypr/scripts/random-wallpaper.sh")
 end)
