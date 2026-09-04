848ad0de1286e9df4585a5ec7aa7d549290e8875
genarold <genarodour2104@gmail.com>
2026-09-04 05:08:32 +0000

    Move all env vars to cursor.lua, env.lua empty

--- .config/hypr/modules/cursor.lua
+++ .config/hypr/modules/cursor.lua
@@ -1,5 +1,21 @@
+-- Cursor
 hl.env("XCURSOR_THEME", "Banana")
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_THEME", "Banana")
 hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("WLR_NO_HARDWARE_CURSORS", "1")
+
+-- Toolkit backend
+hl.env("GDK_BACKEND", "wayland,x11,*")
+hl.env("QT_QPA_PLATFORM", "wayland;xcb")
+hl.env("SDL_VIDEODRIVER", "wayland")
+hl.env("CLUTTER_BACKEND", "wayland")
+
+-- XDG
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("XDG_SESSION_DESKTOP", "Hyprland")
+
+-- Qt
+hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
+hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
